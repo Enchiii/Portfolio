180e3816404fd97276f34832e56b1a554f7313d0
Enchi <eryktobiasz2006@gmail.com>
2025-01-25 00:54:25 +0100

    done

--- frontend/src/components/SingleProject.jsx
+++ frontend/src/components/SingleProject.jsx
@@ -1,22 +1,27 @@
 /* eslint-disable react/prop-types */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import '../styles/SingleProject.css';
 
 // eslint-disable-next-line react/prop-types, no-unused-vars
 const SingleProject = ({ project }) => {
   return (
     <div className="project">
-      {/* <img src={project.image} alt={`${project.title} thumbnail`} className="project-image" /> */}
-      <img src="/Projects/sudoku.png" className='project-image'/>
-      <div className='project-description'>
-        <h3 className='title'>{project.title}</h3>
-        <p>{project.description}</p>
-
+      <div className="project-image-container">
+        {/* Obraz jako link */}
+        <a href={project.github} target="_blank" rel="noopener noreferrer" className="github-overlay">
+          <img src={project.image} alt={`${project.title} thumbnail`} className="project-image" />
+          <FontAwesomeIcon icon={faGithub} className="github-icon" />
+        </a>
+      </div>
+      <div className="project-description">
+        <h3 className="title">{project.title}</h3>
+        <p className="description">{project.description}</p>
         <p className="technologies">
-            {project.technologies.map((tech, index) => (
-                <span key={index} className="technology">{tech}</span>
-            ))}
+          {project.technologies.map((tech, index) => (
+            <span key={index} className="technology">{tech}</span>
+          ))}
         </p>
-
       </div>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,47 @@
+import { useState, useEffect } from "react";
 import TechStack from "../components/TechStack";
+import projectData from '../assets/projectsList.json';
+import SingleProject from "../components/SingleProject";
 import '../styles/Home.css'
+import { Link } from "react-router-dom";
 
 const Home = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        // Ładowanie danych z pliku JSON
+        setProjects(projectData);
+    }, []);
+
     return (
         <>
-        {/* <p className="motto">Linux is only free if your time has no value.</p> */}
-        <p className="motto">Code with purpose, create with passion</p>
+            {/* Motto */}
+            <p className="motto">Code with purpose, create with passion</p>
 
-        <div className="about">
-            <p>Hi, I&apos;m Eryk – a high school student from Kraków, interested in AI and problem-solving.</p>
-            <p>I&apos;m excited about exploring new possibilities in artificial intelligence and using them to create innovative solutions.
-                In addition to programming, I am interested in chess, bouldering and skiing - activities that enhance my strategic thinking, 
-                focus and perseverance. These are qualities that help me in any project.</p>
-            <p>I believe that together, we can create something amazing.</p>
-        </div>
+            {/* Sekcja "O mnie" */}
+            <div className="about">
+                <p>Hi, I&apos;m Eryk – a high school student from Kraków, interested in AI and problem-solving.</p>
+                <p>I&apos;m excited about exploring new possibilities in artificial intelligence and using them to create innovative solutions.
+                    In addition to programming, I am interested in chess, bouldering and skiing - activities that enhance my strategic thinking, 
+                    focus and perseverance. These are qualities that help me in any project.</p>
+                <p>I believe that together, we can create something amazing.</p>
+            </div>
 
-        <TechStack/>
+            {/* Pojedynczy projekt */}
+            <div className="last-project">
+                <h3>One of my last projects</h3>
+                {projects.length > 1 ? (
+                    <SingleProject project={projects[1]} />
+                ) : (
+                    <p>Loading project...</p>
+                )}
+                <p className="more-projects">See more of my projects -&#62; <Link to="/projects" className="link">My projects</Link>  </p>
+            </div>
+
+            {/* Tech stack */}
+            <TechStack />
         </>
-    )
+    );
 }
 
 export default Home;
